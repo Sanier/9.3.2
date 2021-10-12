d54f9ed2ae44808668fc621ebee009600b04e2b3
Sanier <Sanier20@mail.ru>
2021-10-12 19:22:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9._3._2
+{
+    class Program
+    {
+        public delegate int Sum(int a, int b);
+        static void Main(string[] args)
+        {
+            Sum sum = Sumir;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            sum.Invoke(a, b);
+            sum(a, b);
+            Console.ReadKey();
+        }
+
+        static int Sumir(int a, int b)
+        {
+            return a - b; 
+        }
+    }
+}
